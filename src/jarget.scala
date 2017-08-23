1f5cc8036a9d84ffa508e7489a8546d616e0a2af
CaioRS <caiorss.rodrigues@gmail.com>
2017-08-23 09:04:26 -0300

    Add new command line switches to build self-executable uber jars.

--- src/jarget.scala
+++ src/jarget.scala
@@ -524,12 +524,27 @@ Note: The XML in the clipboard is a maven coordinate:
           println("Run  it with: java -jar " + output)
         } 
 
+    case List("jar", "-scala-uber", "-sh", output, main)
+        => {
+          JarBuilder.makeUberJar(output, main, scalaLib = true, executable = true)
+          println("Build file:   " + output)
+          println("Run  it with: ./" + output)
+        } 
+
+
     case "jar"::"-scala-uber"::output::main::"-paths"::paths
         => {
           JarBuilder.makeUberJar(output, main, paths = paths, scalaLib = true)
           println("Build file:   " + output)
           println("Run  it with: java -jar " + output)
         }     
+
+    case "jar"::"-scala-uber"::"-sh"::output::main::"-paths"::paths
+        => {
+          JarBuilder.makeUberJar(output, main, paths = paths, scalaLib = true, executable = true)
+          println("Build file:   " + output)
+          println("Run  it with: java -jar " + output)
+        }   
 
     case "jar"::"-uber"::output::main::"-paths"::paths
         => {
