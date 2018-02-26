515d07c6033b6b63772af011798729c8aa907c9e
CaioRS <caiorss.rodrigues@gmail.com>
2018-02-26 12:38:29 -0300

    Mover jarHeader to resource file assets/unixLoader.sh.

--- src/utils.JarBuilder.scala
+++ src/utils.JarBuilder.scala
@@ -7,39 +7,6 @@ object JarBuilder{
   
   import java.util.jar.{JarOutputStream, JarEntry, JarFile}
 
-  val jarHeader = """#!/usr/bin/env sh 
-# Check if JAVA_HOME is Set 
-if [ -n "${JAVA_HOME}" ]
-then
-    # Check if JAVA is Installed in this JAVA_HOME
-    if [ -f  "$JAVA_HOME/bin/java" ] ;
-    then
-        "$JAVA_HOME/bin/java" -jar "$0" "$@"
-    # Try to use JAVA from $PATH Variable
-    else
-        # Check if Java is Installed 
-        if hash java 2>/dev/null;
-        then
-            java -jar "$0" "$@"
-        else
-            echo "Error: Java not available in PATH variable or in \$JAVA_HOME"
-            echo "Make sure Java is installed"
-            exit 1
-        fi 
-    fi 
-else
-    # Check if Java is Installed 
-    if hash java 2>/dev/null;
-    then
-        java -jar "$0" "$@"
-    else
-        echo "Error: Java not available in PATH variable"
-        echo "Make sure Java is installed"
-        exit 1
-    fi     
-fi
-exit 0
-"""
 
   def addFile(zos: JarOutputStream, entry: String, file: String){
     import java.io._
