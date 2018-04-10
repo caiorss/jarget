c7a5aae21091706c2210822e2a6b80e9c3cf5706
CaioRS <caiorss.rodrigues@gmail.com>
2018-04-10 13:38:19 -0300

    Add public domain header.

--- src/main.scala
+++ src/main.scala
@@ -1,3 +1,31 @@
+/** 
+     - Author: Caio Rodrigues <caiorss [DOT] rodrigues [AT] gmail [DOT] com>
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.  
+
+ =============================================================== */
+
 package jarget.main
 
 import jarget.utils.{Utils, JarUtils}
@@ -222,10 +250,13 @@ object Main{
   val mvnPull = new OptSet(
     name  = "mvn-pull",
     usage = "<PACKAGE1> [<PACKAGE2> ...]",
-    desc  = "Show package's pom.xml file."
-  ).setAction{ res => 
-    val pstr = res.getOperands()(0)
-    showPom(parsePack(pstr)) run repoUrl
+    desc  = "Download package to cache directory.",
+    longDesc = "The package ins the format <group>/<name>/<version>"
+  ).setAction{ res =>
+    tryMVNGet{
+      val packs = res.getOperands() map parsePack
+      Packget.getPackCPathFromCache(packs, cachePath, config.repoUrl)
+    }
   }
 
   val mvnDoc = new OptSet(
